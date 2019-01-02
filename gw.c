c97bbad12dd7dca65c7255931e4628db95250ec5
Nate C-K <ncmadcity@gmail.com>
2019-01-02 14:15:31 -0600

    Fixes.

--- gw.c
+++ gw.c
@@ -9,8 +9,12 @@ const unsigned int LOGICAL_FRAMES_PER_SECOND = 10;
 const unsigned int SCREEN_WIDTH = 800, SCREEN_HEIGHT = 600;
 const char[] WINDOW_TITLE = "GridWalk";
 
+// Used to multiply times in order to increase the precision of tracking the
+// frame duration.
+const Uint32 TIMER_MILLISECOND_MULTIPLIER = 8;
+
 #include "data.c"
-#include "init.c"
+#include "sdl.c"
 #include "timer.c"
 #include "game.c"
 #include "draw.c"
