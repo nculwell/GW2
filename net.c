eadd7e44060e520142a21e681ec1536c5a12a61e
Nate C-K <ncmadcity@gmail.com>
2019-01-02 23:20:46 -0600

    Net changes.

--- net.c
+++ net.c
@@ -2,47 +2,124 @@
 
 #include <enet/enet.h>
 
-static ENetHost* enetClient;
+#define USERNAME "ncmadcity"
+
+#define NET_CONNECTIONS_TO_SUPPORT 2
+#define NET_CHANNELS_TO_SUPPORT 2
+
+enum NetState {
+  NETSTATE_INIT,
+  NETSTATE_BROADCASTING,
+  NETSTATE_CONNECTING,
+  NETSTATE_LOGGING_IN,
+  NETSTATE_LOADING,
+  NETSTATE_PLAYING,
+};
+
+static ENetHost* _enetClient;
+static ENetPeer* _enetServer;
+static enum NetState _netState = NETSTATE_INIT;
 
 void InitNet() {
   if (0 != enet_initialize())
     die("Failed enet_initialize");
   atexit(enet_deinitialize);
-  enetClient = enet_host_create(
-      NULL, // NULL=client
-      1, // connections
-      2, // channels
-      0, // bandwidth
-      0); // bandwidth
-  if (!enetClient)
+  _enetClient = enet_host_create(NULL /* NULL=client */,
+      NET_CONNECTIONS_TO_SUPPORT, NET_CHANNELS_TO_SUPPORT, 0, 0);
+  if (!_enetClient)
     die("Failed to create ENet client");
+  // Broadcast connect request across the network.
+  _enetServer = enet_host_connect(_enetClient, ENET_HOST_BROADCAST,
+      NET_CHANNELS_TO_REQUEST, enet_uint32 data)
+  _netState = NETSTATE_CONNECTING;
+  atexit(CloseNet);
 }
 
-void DeinitNet() {
-  enet_host_destroy(enetClient);
+// _enetServer = enet_host_connect(_enetClient, ENET_HOST_BROADCAST, NET_CHANNELS_TO_REQUEST, enet_uint32 data)
+
+void CloseNet() {
+  enet_host_destroy(_enetClient);
+}
+
+void SendNetMessage(ENetPeer* peer, int msgLen, const char* msg, bool reliable) {
+  ENetPacket* packet = enet_packet_create(
+      msg, msgLen > 0 ? msgLen : 1 + strlen(msg), reliable ? ENET_PACKET_FLAG_RELIABLE : 0);
+  // Send the packet to the peer over channel id 0.
+  // One could also broadcast the packet by
+  // enet_host_broadcast(host, 0, packet);
+  enet_peer_send(peer, 0, packet);
+  enet_host_flush(host);
+}
+
+void SendNetMessageFmt(ENetPeer* peer, bool reliable, const char* fmt, ...) {
+#define MSG_FMT_BUF_SIZE
+  char msg[MSG_FMT_BUF_SIZE];
+  va_list args;
+  va_start(args, format);
+  int msgLen = vsnprintf(msg, MSG_FMT_BUF_SIZE, fmt, args);
+  va_end(args);
+  SendNetMessage(peer, msgLen, msg, reliable);
+}
+
+static bool MsgEq(ENetPacket* packet, const char* compareTo) {
+  return (0 == strncmp(packet->data, compareTo, packet->dataLength));
+}
+
+char* CopyPacketMessage(ENetPacket* packet) {
+  char* messageCopy = Alloc(packet->dataLength);
+  memcpy(messageCopy, packet->data, packet->dataLength);
+  return messageCopy;
+}
+
+void ReceiveNetMessage(ENetEvent* event) {
+  switch (_netState) {
+    case NETSTATE_CONNECTING:
+      // TODO: Look at the packet.
+      // TODO: See if 'reliable' really works for this.
+      SendNetMessageFmt(event->peer, true, "CONNECT %s", USERNAME);
+      _netState = NETSTATE_LOGGING_IN;
+      break;
+    case NETSTATE_LOGGING_IN:
+      if (MsgEq(packet, "WELCOME")) {
+        SendNetMessageFmt(event->peer, true, "LOAD");
+        _netState = NETSTATE_LOADING;
+      }
+      else {
+        die("Unable to log in, server rejected credentials");
+      }
+      break;
+    case NETSTATE_LOADING:
+    case NETSTATE_PLAYING:
+      {
+        char* messageCopy = CopyPacketMessage(packet);
+        RaiseEvent(EVT_NET_MESSAGE, packet->dataLength, messageCopy);
+      }
+      break;
+    default:
+      die("Invalid NetState: %d", _netState);
+  }
+  enet_packet_destroy(event.packet);
 }
 
 void PollNet() {
   ENetEvent event;
-  while (enet_host_service(enetClient, &event, 0) > 0) {
+  while (enet_host_service(_enetClient, &event, 0) > 0) {
     switch (event.type)
     {
       case ENET_EVENT_TYPE_CONNECT:
-        printf("Connected to server: %X port %u.\n", 
+        printf("Connected to server: %X port %u.\n",
             event.peer->address.host, event.peer->address.port);
         // Store any relevant client information here.
         // event.peer->data = "Client information";
         break;
       case ENET_EVENT_TYPE_RECEIVE:
-        printf("A packet of length %u containing %s was received from %s on channel %u.\n",
-            event.packet->dataLength, event.packet->data, event.peer->data,
-            event.channelID);
-        enet_packet_destroy(event.packet);
+        ReceiveNetMessage(&event);
         break;
       case ENET_EVENT_TYPE_DISCONNECT:
-        printf("%s disconnected.\n", event.peer->data);
-        /* Reset the peer's client information. */
-        event.peer->data = NULL;
+        printf("Disconnected.\n");
+        // Reset the peer's client information.
+        // event.peer->data = NULL;
+        break;
     }
   }
 }
